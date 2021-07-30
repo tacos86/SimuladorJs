e78e77b86d4bfe8ce105d80edf55b47ff07d0782
bcosta <49333564j@gmail.com>
2021-07-29 23:37:56 -0300

    agregue texto

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ boton.addEventListener("click", desplegarTexto);
 function desplegarTexto(e) {
   e.target.removeEventListener(e.type, desplegarTexto);
   let texto = document.createElement("p");
-  texto.innerHTML = 'Todas nuestras lanas son %100 acrílicas y estan realizadas con los más altos niveles de calidad. <br> Todas se venden en madejas de 500 grs o en ovillos de 200 grs.'
+  texto.innerHTML = 'Todas nuestras lanas son 100% acrílicas y estan realizadas con los más altos niveles de calidad. <br> Todas se venden en madejas de 500 grs o en ovillos de 200 grs. <br> Hacemos envíos en CABA y GBA.'
   document.body.appendChild(texto);
 
   texto.style.textAlign = "center";
