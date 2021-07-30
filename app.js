a1c2d8171290a070b2f3f11bb0d6009756b18b93
bcosta <49333564j@gmail.com>
2021-07-29 23:17:20 -0300

    desafio 9 - eventos

---
+++ app.js
@@ -0,0 +1,26 @@
+let color = document.getElementById("color");
+let cantidad = document.getElementById("cantidad");
+console.log(cantidad)
+
+let boton = document.getElementById("boton");
+boton.addEventListener("click", desplegarTexto);
+
+function desplegarTexto(e) {
+  e.target.removeEventListener(e.type, desplegarTexto);
+  let texto = document.createElement("p");
+  texto.innerHTML = 'Todas nuestras lanas son %100 acrílicas y estan realizadas con los más altos niveles de calidad. <br> Todas se venden en madejas de 500 grs o en ovillos de 200 grs.'
+  document.body.appendChild(texto);
+
+  texto.style.textAlign = "center";
+    
+}
+
+
+
+
+ 
+
+
+
+
+
